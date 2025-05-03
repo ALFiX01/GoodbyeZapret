0a9f8e811ae75185bc541f7c9f3cab8c99904e21
ALFiX <boyarov.danil71@mail.ru>
2025-05-03 22:53:36 +0300

    Обновлены скрипты UltimateFix_Amaizing.bat и WebUnlock_X-ip.bat для улучшения параметров фильтрации. Добавлен новый скрипт curl_test.bat для проверки доступности сервисов.

--- Project/Configs/UltimateFix_Amaizing.bat
+++ Project/Configs/UltimateFix_Amaizing.bat
@@ -13,16 +13,19 @@ set "BIN=%parentDir%bin\"
 set "LIST_TITLE=GoodbyeZapret: UltimateFix Amaizing"
 set "LISTS_FOLDER=%parentDir%lists"
 REM BEST
-REM --dpi-desync=fake --dpi-desync-fooling=badseq --dpi-desync-fake-http=0x00000000
+REM preview      --dpi-desync=fake --dpi-desync-fooling=badseq --dpi-desync-fake-http=0x00000000
+REM --dpi-desync=fake --dpi-desync-ttl=2 --dpi-desync-fake-tls-mod=rnd,dupsid,rndsni,padencap
+
 REM --dpi-desync=fake --dpi-desync-fooling=datanoack --dpi-desync-fake-http=0x00000000 
 REM --dpi-desync=fakedsplit --dpi-desync-ttl=1 --dpi-desync-autottl=3 --dpi-desync-split-pos=1
 
 start "%LIST_TITLE%" /min "%BIN%winws.exe" ^
 --wf-tcp=80,443 --wf-udp=443,50000-50099 ^
---filter-udp=50000-50099 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=5 --new ^
---filter-tcp=443 --hostlist="%LISTS_FOLDER%\youtube_video-preview.txt" --dpi-desync=fake --dpi-desync-fooling=badseq --dpi-desync-fake-http=0x00000000 --new ^
+--filter-udp=50000-50099 --filter-l7=discord,stun --dpi-desync=fake --new ^
+--filter-tcp=443 --hostlist="%LISTS_FOLDER%\youtube_video-preview.txt" --dpi-desync=fake --dpi-desync-ttl=2 --dpi-desync-fake-tls-mod=rnd,dupsid,rndsni,padencap --new ^
+--filter-tcp=443 --ipset="%LISTS_FOLDER%/russia-youtube-rtmps.txt" --dpi-desync=syndata,multisplit --dpi-desync-fake-syndata="%BIN%/tls_clienthello_4.bin" --dpi-desync-split-pos=1 --new ^
 --filter-udp=443 --hostlist="%LISTS_FOLDER%\youtubeQ.txt" --dpi-desync=fake,fakeddisorder --dpi-desync-ttl=1 --dpi-desync-autottl=4 --dpi-desync-split-pos=method+2 --dpi-desync-fake-http=0x00000000 --new ^
---filter-tcp=443 --hostlist="%LISTS_FOLDER%\list-youtube.txt" --dpi-desync=fakedsplit --dpi-desync-ttl=1 --dpi-desync-autottl=3 --dpi-desync-split-pos=1 --new ^
+--filter-tcp=443 --hostlist="%LISTS_FOLDER%\list-youtube.txt" --dpi-desync=fakedsplit --dpi-desync-ttl=2 --dpi-desync-split-pos=1 --new ^
 --filter-udp=443 --ipset="%LISTS_FOLDER%\ipset-cloudflare.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --hostlist-exclude="%LISTS_FOLDER%\exclude.txt" --new ^
 --filter-tcp=80 --ipset="%LISTS_FOLDER%\ipset-cloudflare.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-exclude="%LISTS_FOLDER%\exclude.txt" --new ^
 --filter-tcp=443 --ipset="%LISTS_FOLDER%\ipset-cloudflare.txt" --dpi-desync=fake,split2 --dpi-desync-repeats=6 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls="%BIN%tls_clienthello_www_google_com.bin" --hostlist-exclude="%LISTS_FOLDER%\exclude.txt"
--- Project/Tools/curl_test.bat
+++ Project/Tools/curl_test.bat
@@ -1,32 +1,29 @@
 @echo off
-
-SET URL_GOOGLEVIDEO=https://rr5---sn-jvhnu5g-c35d.googlevideo.com
-SET URL_DISCORD=https://discord.com
-
 rem --- Проверка googlevideo ---
-echo  Проверка Youtube...
+echo.
+echo   Проверка Youtube...
 
 rem -o nul: перенаправить вывод заголовков в пустоту (чтобы не засорять консоль)
-curl -s -L -I --connect-timeout 1 -o nul %URL_GOOGLEVIDEO%
+curl -s -L -I --connect-timeout 1 -o nul https://rr5---sn-jvhnu5g-c35d.googlevideo.com
 
 rem Проверяем код завершения команды curl. 0 обычно означает успех.
 IF %ERRORLEVEL% EQU 0 (
-    echo   Доступен.
+    echo    Доступен.
 ) ELSE (
-    echo   НЕДОСТУПЕН ^(код ошибки curl: %ERRORLEVEL%^).
+    echo    НЕДОСТУПЕН ^(код ошибки curl: %ERRORLEVEL%^).
 )
 echo.
 
 rem --- Проверка discord.com ---
 echo  Проверка discord...
-curl -s -L -I --connect-timeout 1 -o nul %URL_DISCORD%
+curl -s -L -I --connect-timeout 1 -o nul https://discord.com
 
 IF %ERRORLEVEL% EQU 0 (
-    echo   Доступен.
+    echo    Доступен.
 ) ELSE (
-    echo   НЕДОСТУПЕН ^(код ошибки curl: %ERRORLEVEL%^).
+    echo    НЕДОСТУПЕН ^(код ошибки curl: %ERRORLEVEL%^).
 )
 echo.
 
-echo Проверка завершена.
+echo  Проверка завершена.
 
